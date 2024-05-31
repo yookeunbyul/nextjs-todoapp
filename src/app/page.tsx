7cf5bd973f7b19b979134c035ddc1f29c6eda117
yookeunbyul <si89336@naver.com>
2024-05-31 17:25:57 +0900

    💄 Design: todo input design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold">Hello Todo App!</h1>
+      <div className="w-full bg-indigo-50 text-center min-h-screen">
+        <h1 className="text-5xl font-bold pt-10 pb-5">Hello Todo App!</h1>
+        <div className="p-10">
+          <input
+            className="w-5/6 placeholder:text-gray-400 border-0 p-5 rounded-l-lg focus:outline-none"
+            placeholder="your todo here"
+          />
+          <button className="w-1/6 bg-indigo-200 rounded-r-lg p-5">
+            Submit!
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
