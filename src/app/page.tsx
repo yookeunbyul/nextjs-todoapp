bad92b544204ec79c3af7f7d91d199960ab945aa
yookeunbyul <si89336@naver.com>
2024-06-06 17:27:45 +0900

    💄 Design: react-icons 추가 및 반응형

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ import { useState } from "react";
 import {
   MdOutlineCheckBoxOutlineBlank,
   MdOutlineCheckBox,
+  MdRocketLaunch,
+  MdCheck,
+  MdDelete,
+  MdEdit,
 } from "react-icons/md";
 
 export default function Home() {
@@ -22,7 +26,7 @@ export default function Home() {
     <main>
       <div className="w-full bg-indigo-50 text-center min-h-screen">
         <h1 className="text-5xl font-bold pt-10 pb-5">Hello Todo App!</h1>
-        <div className="p-10">
+        <div className="p-10 flex items-center">
           <input
             className="w-5/6 placeholder:text-gray-400 border-0 p-5 rounded-l-lg focus:outline-none"
             placeholder="your todo here"
@@ -33,7 +37,9 @@ export default function Home() {
             className="w-1/6 bg-indigo-200 rounded-r-lg p-5"
             onClick={handleAdd}
           >
-            Submit!
+            <div className="flex items-center justify-center">
+              <MdRocketLaunch size="24" />
+            </div>
           </button>
         </div>
 
@@ -52,7 +58,7 @@ export default function Home() {
                 )}
               </div>
               <input
-                className="flex-1 placeholder:text-gray-400 border-0 p-5 rounded-lg focus:outline-none"
+                className="flex-1 placeholder:text-gray-400 border-0 p-5 rounded-lg focus:outline-none w-16 md:w-32 lg:w-48"
                 disabled={todo.isDisabled}
                 value={todo.text}
                 onChange={(e) => update(todo.id, e.target.value)}
@@ -62,21 +68,27 @@ export default function Home() {
                   className="flex-none mx-4 bg-indigo-200 p-5 rounded-lg"
                   onClick={() => updateDisabled(todo.id, false)}
                 >
-                  edit
+                  <div className="flex items-center justify-center">
+                    <MdEdit size="30" />
+                  </div>
                 </button>
               ) : (
                 <button
                   className="flex-none mx-4 bg-indigo-200 p-5 rounded-lg"
                   onClick={() => updateDisabled(todo.id, true)}
                 >
-                  done
+                  <div className="flex items-center justify-center">
+                    <MdCheck size="30" />
+                  </div>
                 </button>
               )}
               <button
                 className="flex-none bg-indigo-200 p-5 rounded-lg"
                 onClick={() => remove(todo.id)}
               >
-                delete
+                <div className="flex items-center justify-center">
+                  <MdDelete size="30" />
+                </div>
               </button>
             </div>
           ))}
